ce11f6344cc4764d4c07b13c4b5829bad287dad2
Celso Bertolotto <celsonyl@hotmail.com>
2020-09-03 20:48:01 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+            
+            Console.WriteLine("Entre com as medidas do triangulo X!");
+             x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+             x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+             x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triangulo Y!");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("Area de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX == areaY)
+            {
+               Console.WriteLine("As duas áreas são iguais!");
+            }else if(areaX > areaY){
+                Console.WriteLine("Maior área: X ");
+            }else {
+                Console.WriteLine("Maior área: Y ");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Primeiro/Triangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primeiro
+{
+    class Triangulo{
+
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+   
+        }
+    }
+}
